6650674b4b4f7d23646d077caa536034ecf70b8a
aminzer <minzer.alexey@gmail.com>
2024-09-15 11:57:30 +0300

    set base url in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/tic-tac-toe/', 
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
