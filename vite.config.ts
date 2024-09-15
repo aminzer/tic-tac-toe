6449f5830331e492a73b29fec8e6a0cc4df31ea4
aminzer <minzer.alexey@gmail.com>
2024-09-15 14:54:48 +0300

    set base path based on Vite mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
-import path from "path"; 
+import path from "path";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/tic-tac-toe/', 
-  plugins: [
-    react({
-      jsxImportSource: "@emotion/react",
-      babel: {
-        plugins: [
-          "@emotion/babel-plugin",
-        ],
+export default defineConfig(({ mode }) => {
+  const basePath = mode === 'production' ? '/tic-tac-toe/' : '/';
+
+  return {
+    base: basePath,
+    plugins: [
+      react({
+        jsxImportSource: "@emotion/react",
+        babel: {
+          plugins: [
+            "@emotion/babel-plugin",
+          ],
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@app": path.resolve(__dirname, "./src"),
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@app": path.resolve(__dirname, "./src"),
     },
-  },
+  }
 });
