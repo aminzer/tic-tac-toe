47489a673d0b3e696dc92d8b429b295b786cd3b4
aminzer <minzer.alexey@gmail.com>
2024-12-03 16:57:36 +0300

    use env vars to set base URL path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import path from "path";
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const basePath = mode === 'production' ? '/tic-tac-toe/' : '/';
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+  const baseUrlPath = env.VITE_BASE_URL_PATH ?? '/'
 
   return {
-    base: basePath,
+    base: baseUrlPath,
     plugins: [
       react({
         jsxImportSource: "@emotion/react",
