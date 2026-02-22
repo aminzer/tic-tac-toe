4e51400fa87d67e3f3ad534651d4917654b4bcd1
aminzer <minzer.alexey@gmail.com>
2026-02-22 22:45:15 +0300

    enable React compiler

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
         jsxImportSource: "@emotion/react",
         babel: {
           plugins: [
+            "babel-plugin-react-compiler",
             "@emotion/babel-plugin",
           ],
         },
